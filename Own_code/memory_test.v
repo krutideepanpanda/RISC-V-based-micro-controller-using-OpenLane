800d8ed189a1405715cb0f541029ab25df3552c1
krutideepanpanda <krutideepan123@gmail.com>
2022-04-06 15:07:32 +0530

    Tidy Up

--- Own_code/memory_test.v
+++ Own_code/memory_test.v
@@ -11,26 +11,26 @@ module memory_test();
     
     Common_Memory test(.clk(clk), .Adr(Adr), .MWD(MWD), .MWR(MWR), .MOE(MOE), .MRD(MRD));
     
-    always #10 clk = ~clk;
+    always #25 clk = ~clk;
     
     initial
     begin
         clk <=0;
-        #(10);
+        #(25);
         
         Adr <= 9'd10;
         MWD <= 32'b1;
         MWR <=1;
         MOE <=0;
         
-        #(10);
+        #(25);
         
         Adr <= 9'b0;
         MWR <=1;
         MOE <=0;
         MWD <= 32'b1;
         
-        #(10);
+        #(25);
         Adr <= 9'd10;
         MWR <=0;
         MOE <=1'b1;
